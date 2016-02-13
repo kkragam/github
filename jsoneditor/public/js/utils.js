689050c7b60a79a04aec64458079c7bd4a5697c4
kiran <kiran1.kumar1@gmail.com>
2016-02-13 15:59:49 +0800

    added 2 col layout

--- jsoneditor/public/js/utils.js
+++ jsoneditor/public/js/utils.js
@@ -145,6 +145,30 @@ function iterateAttributesAndFormHTMLLabels(obj){
 
 
 
+
+function dataBind(domElement, obj) {
+
+    var bind = domElement.getAttribute("bind").split(":");
+    var domAttr = bind[0].trim();
+    var itemAttr = bind[1].trim();
+
+    Object.observe(obj, function (change) {
+        domElement[domAttr] = obj[itemAttr];
+    });
+    new MutationObserver(updateObj).observe(domElement, {
+        attributes: true,
+        childList: true,
+        characterData: true
+    });
+    domElement.addEventListener("keyup", updateObj);
+    domElement.addEventListener("click",updateObj);
+    function updateObj(){
+        obj[itemAttr] = domElement[domAttr];   
+    }
+    domElement[domAttr] = obj[itemAttr];
+}
+
+
 //Public access
 return{
     loadMenuItem:loadMenuItem,
